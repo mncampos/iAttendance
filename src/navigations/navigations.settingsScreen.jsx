fdfbcc90ac72b99c0f60f5d9e0a3a1f62a4d1751
mncampos <mncampos@inf.ufrgs.br>
2021-10-20 18:21:20 -0300

    logout

--- src/navigations/navigations.settingsScreen.jsx
+++ src/navigations/navigations.settingsScreen.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { Button } from 'react-native-paper'
 
-export function SettingsScreen()
+export function SettingsScreen( {navigation} )
 {
     return(
         <View>
+        <View>
             <Text> Configurações </Text>
+        </View>
+
+        <View>
+            <Button icon='logout' onPress={ () => navigation.navigate('LoginScreen'  )}>
+            Logout
+            </Button>
+        </View>
         </View>
     );
 }
